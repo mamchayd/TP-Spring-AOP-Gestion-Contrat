e28515f04892e20d160910eb65ee922eec8860ee
Amchayd Mariam <meriam.amchayd@gmail.com>
2021-01-26 12:26:35 +0100

    first commit

---
+++ src/test/java/org/enset/contrat/ContratApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enset.contrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
